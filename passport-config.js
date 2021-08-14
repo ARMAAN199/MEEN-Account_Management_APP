eabbac83a38a6ef6315cb6aa543a38608b6c7f6e
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-14 12:01:01 +0530

    transactions

--- passport-config.js
+++ passport-config.js
@@ -53,3 +53,22 @@ passport.use('local-signup', new LocalStrategy({
                 return done(null, newUser);
             });
         }
+        
+      });
+
+    });
+  
+  }));
+  
+  
+  
+  passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, email, password, done) { // callback with email and password from our form
+  
+    // find a user whose email is the same as the forms email
+    // we are checking to see if the user trying to login already exists
