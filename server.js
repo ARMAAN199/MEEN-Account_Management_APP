aaa71acab641d265eabfe37194588c2111ee7a3b
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-16 12:01:01 +0530

    transactions

---
+++ server.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const User = require("./models/userSchema");
+const Transaction = require("./models/transactionSchema");
+const objparser = require("bson-objectid");
