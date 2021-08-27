8dcb6fcb5e9da3ff885bee8b76bf590d9661714a
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-27 12:01:01 +0530

    transactions

--- server.js
+++ server.js
@@ -13,3 +13,37 @@ const mongoose = require("mongoose");
 const User = require("./models/userSchema");
 const Transaction = require("./models/transactionSchema");
 const objparser = require("bson-objectid");
+const Customer = require("./models/customerSchema");
+const url =
+  "mongodb+srv://armaan199:abcd@cluster0.0gdku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var config = require("./passport-config")(passport, bcrypt); // pass passport for configuration
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+
+app.use(express.static(__dirname + "/public"));
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: "process.env.SESSION_SECRET",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
