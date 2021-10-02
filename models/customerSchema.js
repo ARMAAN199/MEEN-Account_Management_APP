e5e17b74e035d8ec6fc4e69572af5a1c03f1ab69
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-10-02 08:04:15 +0530

    transactions

--- models/customerSchema.js
+++ models/customerSchema.js
@@ -16,6 +16,24 @@ var customerschema = mongoose.Schema({
     required: true,
     default: 0,
   },
+  initialBal: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  sub: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  sub2: {
+    type: String,
+    required: false,
+  },
+  sub3: {
+    type: Number,
+    required: false,
+  },
 });
 
 module.exports = mongoose.model("Customer", customerschema);
