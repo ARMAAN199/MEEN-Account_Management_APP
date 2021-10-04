c118ba703d784ef1ce1c903160c3ca8e06623d05
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-10-04 08:04:15 +0530

    transactions

--- models/transactionSchema.js
+++ models/transactionSchema.js
@@ -1,15 +1,45 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var transactionSchema = mongoose.Schema({
-    userid: String,
-    customerid: String,
-    date:String,
-    type: {
-        type: Number,
-        required: true,
-        default: 0    
-    },
-    amount: {
-        type: Number,
-        required: true        
-    }, 
+  userid: String,
+  customerid: String,
+  date: String,
+  type: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  mode: {
+    type: String,
+    default: "cash",
+  },
+  bill: {
+    hasbill: Boolean,
+    default: 0,
+    billdetails: [
+      {
+        billdate: String,
+        billnumber: Number,
+      },
+    ],
+  },
+  sub: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  sub2: {
+    type: String,
+    required: false,
+  },
+  sub3: {
+    type: Number,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
